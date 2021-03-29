6cd794f58118d2eadf1e78820ad88df990a03dd4
Muru Raj <muru.raj10@gmail.com>
2021-03-29 17:47:21 +1300

    Update data_AE_2.py

--- data_AE_2.py
+++ data_AE_2.py
@@ -1,4 +1,4 @@
-#cleaning data
+#similar code to deployment but with graphs
 
 import pandas as pd
 import numpy as np
@@ -21,12 +21,8 @@ import matplotlib.pyplot as plt
 #from scipy import stats
 from sklearn.metrics import roc_auc_score, average_precision_score
 
-#from collections import defaultdict
-#from datetime import date, datetime
-#import warnings
-#from itertools import combinations
 
-folder = '/home/nuc/Desktop/PythonProgs/FlowRecorder/DuncanData/Full_Data/'
+
 
 #split the dataset to morning and night here!
 
@@ -35,10 +31,6 @@ test_data = pd.read_csv(folder+'anomaly_data_latency.csv')
 
 norm_data2 = pd.read_csv(folder+'normal_data2.csv')
 test_data2 = pd.read_csv(folder+'anomaly_data_battery.csv')
-#1) During day time, battsV<13V is anomaly. During night <12.5V is anomaly. Night is statenum=3
-#2) battV-BattsenseV>0.1 is anomaly
-#3) take out error data 159.87
-#4) column C >14.4 is anom?
 
 norm_data = CreateChangeFeature(norm_data,['battsV', 'battsSensedV','arrayV', 'arrayI','hsTemp', 'rtsTemp', 'outPower', 'inPower'])
 norm_data2 = CreateChangeFeature(norm_data2,['battsV', 'battsSensedV','arrayV', 'arrayI','hsTemp', 'rtsTemp', 'outPower', 'inPower'])
@@ -46,11 +38,10 @@ test_data = CreateChangeFeature(test_data,['battsV', 'battsSensedV','arrayV', 'a
 test_data2 = CreateChangeFeature(test_data2,['battsV', 'battsSensedV','arrayV', 'arrayI','hsTemp', 'rtsTemp', 'outPower', 'inPower'])
 
 #normal operations data taken. those that have a few discrepancies are removed for training.
-df = pd.concat([norm_data2[:1300],norm_data2[1600:3200],norm_data2[3500:3800],norm_data2[4400:5100],norm_data2[5600:5900],norm_data2[7100:8400],norm_data2[10200:10800],norm_data2[11800:12500]]).reset_index(drop=True)
-df2 = pd.concat([test_data2,norm_data2[6050:6350]]).reset_index(drop=True)
+df = norm_data2
+df2 = test_data2
 
 norm_data_full = norm_data.append(df,ignore_index=True).reset_index(drop=True)
-#test_data_full = test_data.append(test_data2,ignore_index=True).reset_index(drop=True)
 test_data_full = test_data.append(df2,ignore_index=True).reset_index(drop=True)
 
 
@@ -69,12 +60,7 @@ def ConvertToDateTime(df):
 
 norm_data_full = ConvertToDateTime(norm_data_full)
 test_data_full = ConvertToDateTime(test_data_full)
-added_norm = test_data_full[-298:]['DateTime'] #removed 2 bad points
 
-#still checking on 'signal', 'TXLatency'
-#'TXRate', 'RXRate' have some problems in the test file. Removed for now. (check!?!)
-#List of numerical attributes in the data. Names need to match the dataset
-#try removing 'battsV', 'battsSensedV', 'latency'
 numerical = ['battsI', 'arrayV', 'arrayI',
        'v_target', 'hsTemp', 'rtsTemp', 'outPower', 'inPower', 'sweep_pmax',
        'sweep_vmp', 'sweep_voc', 'minVb_daily', 'maxVb_daily', 'ahc_daily',
@@ -83,12 +69,8 @@ numerical = ['battsI', 'arrayV', 'arrayI',
         'NoiseFloor', 'ch_arrayV',
        'ch_arrayI', 'ch_hsTemp', 'ch_rtsTemp', 'ch_outPower', 'ch_inPower','TXLatency','latency'] #'battsV', 'battsSensedV','ch_battsV', 'ch_battsSensedV'
 
-#'flags_daily' is removed for now
-#list of nominal variables in the data
 nominal = ['statenum','weather_long']
-
-#list of indicator variables which have a fixed value for data #removed 'freq'
-fixed_cols = ['dipswitches','CCQ'] #fixed value. anomaly if changes
+fixed_cols = ['dipswitches','CCQ'] 
 
 #convert datatype in nominal variables to string if they are not already
 for col in nominal:
@@ -96,21 +78,11 @@ for col in nominal:
     test_data_full[col] = test_data_full[col].astype(str)
 
 
-#remove rows with latency above 10 and TXLatency above 10. Latency values above 10 are anomalies that we want to detect.
-norm_data_full = norm_data_full.drop(norm_data_full[norm_data_full['latency']>10].index).reset_index(drop=True)
-norm_data_full = norm_data_full.drop(norm_data_full[norm_data_full['TXLatency']>10].index).reset_index(drop=True)
-
 #label anomaly data
 test_data_full['label'] = 0
-#test_data_full['type']='normal'
 test_data_full.loc[test_data_full[test_data_full['latency']>10].index,'label']=1
 labels_latency = test_data_full['label']
-#test_data_full.loc[test_data_full[test_data_full['latency']>10].index,'type']='latency'
 
-#test_data_full.loc[test_data_full[test_data_full['latency']>10].index,'label']=1
-#test_data_full.loc[test_data_full[test_data_full['???']>10].index,'type']='battery'
-
-#create binary feature for fixed values
 CheckUnique(norm_data_full,fixed_cols)
 
 vals = {}
@@ -120,11 +92,6 @@ for col in fixed_cols:
 norm_data = CreateBinary(norm_data_full,fixed_cols,vals)
 test_data = CreateBinary(test_data_full,fixed_cols,vals)
 
-#check normal data in test dataset
-#des_norm_train = norm_data.describe()
-#des_norm_test = test_data[test_data['label']==0].describe()
-#des_diff = des_norm_train - des_norm_test #numbers should be small for all columns
-#print(des_diff)
 #######################################################################################################################3
 #split data into time periods
 def Split_by_Time(df,starth,endh):
@@ -164,24 +131,12 @@ norm_data_enc = encode_onehot(norm_data, nominal)
 test_data_enc = encode_onehot(test_data, nominal)
 
 Check_same_values(norm_data,test_data,norm_data_enc,test_data_enc,colms=nominal)
-#check if need to merge some colums, eg, freq, CCQ, dipswitches
 
 #list of binary variables after one hot encoding
 binary = list(norm_data_enc.columns[list(norm_data_enc.columns).index('Time')+1:])
 
 CheckUnique(norm_data_enc,binary) #should not produce any output
 CheckUnique(test_data_enc,binary)
-
-
-#in this dataset, CCQ=333, dipswitches=101111 and freq=5645 has only one value in both training and testing set
-#so pointless but we are still including it here for completeness
-
-########################################################################################################################
-#create new feature
-
-#delta_features=['ahc_daily']  #currently not in use
-#norm_data_enc = CreateChangeFeature(norm_data_enc,delta_features)
-#numerical.extend(list(norm_data_enc.columns)[-len(delta_features):])
 
 
 
@@ -357,13 +312,6 @@ df_sorted = test_data.sort_values('DateTime').reset_index(drop=True)
 df_sorted['log_scores'] = np.log(df_sorted['scores'])
 df_sorted['log_scores'] = df_sorted['log_scores'] + np.abs(np.min(df_sorted['log_scores']))
 
-#sort_scores = sorted(df_sorted['scores'])
-#df_sorted[df_sorted['log_scores']>20]
-#reducing very large values for figure
-#for i in range(len(df_sorted)):
-#    if df_sorted.loc[i,'log_scores']>20:
-#        df_sorted.loc[i,'log_scores'] = df_sorted.loc[i,'log_scores'] - 130
-
 df_sorted.to_csv('scored_test2.csv')
 
 #ranking of features for latency anomalies. Not very good cause the 'latency' was introduced to detect
@@ -377,7 +325,6 @@ df_rank_latency = pd.DataFrame()
 df_rank_latency['features'] = cols
 df_rank_latency['chi'] = chi
 df_rank_latency['minfo'] = minfo
-#df_rank_latency = df_rank_latency.sort_values('chi').reset_index(drop=True)
 df_rank_latency = df_rank_latency.sort_values('minfo').reset_index(drop=True)
 
 #make sure latency anomalies are not detectable here.
@@ -535,47 +482,6 @@ ax11.set_ylabel('RTT')
 ax11.grid(True)
 
 plt.savefig('scores_latency2.png')
-
-
-
-"""
-plt.clf()
-fig, axs = plt.subplots(2, 1,figsize=(25,8))
-df_sorted['baseline'] = 3.5
-import matplotlib.dates as mdates
-axs[0].plot('DateTime','log_scores',data=df_sorted.loc[688:],marker='x', markerfacecolor='black', color='red', linewidth=1 )
-axs[0].plot('DateTime','baseline', data=df_sorted.loc[688:],color='blue')
-
-axs[1].plot('DateTime','battsV',data = df_sorted.loc[688:],marker='.', markerfacecolor='black', color='green', linewidth=1)
-plt.show()
-
-plt.savefig('batts_norm_anom.png')
-"""
-
-
-
-#label anomaly data
-#test_data_full['label'] = 0
-#test_data_full['type']='normal'
-#test_data_full.loc[test_data_full[test_data_full['latency']>10].index,'label']=1
-#test_data_full.loc[test_data_full[test_data_full['latency']>10].index,'type']='latency'
-
-#test_data_full.loc[test_data_full[test_data_full['latency']>10].index,'label']=1
-#test_data_full.loc[test_data_full[test_data_full['???']>10].index,'type']='battery'
-
-
-#labels, scores = zip(*idx_label_score)
-#labels1 = np.array(labels)
-#labels = np.array(labels.replace(to_replace=-1,value=0))
-#scores = np.array(scores)
-
-#print('AE Test AUC = {}'.format(roc_auc_score(labels, scores)))
-#print('AE Average Precision = {}'.format(average_precision_score(labels,scores)))
-
-#k = evaluate_rate(classify_pts(labels,scores))
-#print('AE results opt thres fpr:{:.2f}%, tpr:{:.2f}%, acc:{:.2f}%, prec:{:.2f}%, f1:{:.2f}%'.format(
-#    100 * k[0], 100 * k[1], 100 * k[2], 100 * k[3], 100 * k[4]))
-
 
 
 
